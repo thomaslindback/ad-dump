9accc2b596162b6afecbc639f0860dfd0e507ecf
Thomas Lindbäck <thomas.lindback@gmail.com>
2018-02-13 18:34:24 +0100

    initial commit

---
+++ src/main/java/org/teel/ad/Company.java
@@ -0,0 +1,9 @@
+package org.teel.ad;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "company")
+public class Company {
+}
---
+++ src/main/java/org/teel/ad/User.java
@@ -0,0 +1,9 @@
+package org.teel.ad;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+}
